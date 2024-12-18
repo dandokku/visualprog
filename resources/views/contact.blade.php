bf546f3eb6cb273d96d7158d59f6026b3b6e8e5a
Dandokku <jesulobadaniel1@gmail.com>
2024-12-18 13:17:52 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Class </title>
+    <link rel="stylesheet" href="../css/navbar.css">
+</head>
+<body class="shii">
+
+    <!--------------------------------------- NAVIGATION BAR --------------------------------------------->
+
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+
+    This is the Contact Page
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../visualprog/resources/css/navbar.css">
+    <title>Laravel Class </title>
+</head>
+<body class="shii">
+
+    <!--------------------------------------- NAVIGATION BAR --------------------------------------------->
+
+    <ul class="links">
+        <li><a href="/" class="link">Home</a></li>
+        <li><a href="/about" class="link">About</a></li>
+        <li><a href="/services" class="link">Services</a></li>
+        <li><a href="/contact" class="link">Contact</a></li>
+    </ul>
+
+    This is the Home Page
+</body>
+</html>
